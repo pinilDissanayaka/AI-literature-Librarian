b05421d861ab623726b1433948a8299b91f5d331
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-19 14:52:31 +0530

    add the web search tool

--- tools/tools.py
+++ tools/tools.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from langchain_community.tools import TavilySearchResults
+from langchain.tools import tool
 
 load_dotenv(find_dotenv(filename=".env"))
 
@@ -8,7 +9,7 @@ load_dotenv(find_dotenv(filename=".env"))
 os.environ["TAVILY_API_KEY"]=os.getenv("TAVILY_API_KEY")
 
 
-
+@tool("Web Search tool")
 def web_search(search_quary:None,max_results=7, include_images=True):
     if os.environ["TAVILY_API_KEY"] == None:
         raise ValueError("Web search can not be perform that time.")
