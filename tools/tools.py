727ea1e0caa8d1bdec7f4f0e80f0496361456620
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-19 10:54:40 +0530

    add search tool

--- tools/tools.py
+++ tools/tools.py
@@ -1,1 +1,16 @@
 import os
+from dotenv import load_dotenv, find_dotenv
+from langchain_community.tools import TavilySearchResults
+
+load_dotenv(find_dotenv(filename=".env"))
+
+
+os.environ["TAVILY_API_KEY"]=os.getenv("TAVILY_API_KEY")
+
+
+
+s=TavilySearchResults()
+
+o=s.invoke("site:google.comAi")
+
+print(o)
